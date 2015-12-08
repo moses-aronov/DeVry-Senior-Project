3d7c9accf91aca3b748f3aebdafeaf336e4f5d50
Moses Aronov <moses@aronov.me>
2015-12-08 00:12:52 -0500

    Created AlertColor Variable used for alert colors. Created setUserFeedback method for errors and messages for users

--- FinalProject/master.Master.cs
+++ FinalProject/master.Master.cs
@@ -9,9 +9,36 @@ namespace FinalProject
 {
     public partial class master : System.Web.UI.MasterPage
     {
+        //CSS Class Variable for Error AlertColor
+        public string AlertColor = "";
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+           
+        }
+        
+        //Set User Feedback using string as input
+        public void setUserFeedback (string message, string alert)
+        {
+            try
+            {
+                //Set lblUserFeedback
+                lblUserFeedback.Text = message;
+                AlertColor = alert;
+            }
+            catch (Exception error)
+            {
+                //Create error message if there is a problem passing arguments.
+                lblUserFeedback.Text = "Error: " + error.Message;
+                AlertColor = "danger";
+            }
+        }
+        
+        //Set UserFeedBack Function using dictionary object
+        public void setUserFeedback(Dictionary<string, string> userFeedback)
+        {
+            //Pass dictionary to setUserFeedback
+            setUserFeedback(userFeedback["message"], userFeedback["alert"]);
         }
     }
 }
