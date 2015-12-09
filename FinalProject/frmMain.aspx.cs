91dd53eb85c8d35decd8f069f3de464e1698bdae
Lbenta <benta.lem@gmail.com>
2015-12-08 23:35:38 -0500

    Copied over frmMain.aspx.cs

--- FinalProject/frmMain.aspx.cs
+++ FinalProject/frmMain.aspx.cs
@@ -7,9 +7,33 @@ using System.Web.UI.WebControls;
 
 namespace FinalProject
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class frmMain : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+            clsDataLayer.SaveUserActivity(Server.MapPath("FinalProject_DB.accdb"), "frmPersonnel");
+            if (Session["SecurityLevel"] == "A")
+            {
+                imbtnStaff.Visible = false;
+                LinkButtonStaff.Visible = false;
+
+
+            }
+
+            if (Session["SecurityLevel"] == "S")
+            {
+                LinkButtonManage.Visible = false;
+                imgbtnManage.Visible = false;
+
+            }
+
+
+
+        }
+
+
+
+        protected void LinkButtonManage_Click(object sender, EventArgs e)
         {
 
         }
